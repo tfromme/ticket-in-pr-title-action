d14c6c6c78967b202c2eb43368b38b32cc27cc9d
Tim Fromme <tfromme@cytracom.com>
2022-05-31 23:23:44 -0400

    Initial Action

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ticket-in-pr-title-action
 Github Action to Check if a JIRA Ticket is mentioned in the PR Title
+
+## Inputs
+  ticket-prefix - Ticket Prefix to search for
+
+  fail - Whether to fail (default) or just warn
+
+  pr-title - Title of the PR (default ${{ github.event.pull_request.title }})
+
+## To Build
+You must have ncc installed `npm i -g @vercel/ncc`
+Then run `bash build.sh`
---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+
+async function runAction({ ticketPrefix, prTitle, fail }) {
+  const re = new RegExp(`${ticketPrefix}-[1-9][0-9]*`);
+
+  if (!re.test(prTitle)) {
+    const errorMessage = `No Tickets matching ${ticketPrefix} found in PR Title.`;
+    if (fail) {
+      core.setFailed(errorMessage);
+    } else {
+      core.warning(errorMessage);
+    }
+  }
+}
+
+function parseBool(str) {
+  return str === 'true' || str === true;
+}
+
+function parseInputs() {
+  return {
+    ticketPrefix: core.getInput('ticket-prefix'),
+    prTitle: core.getInput('pr-title'),
+    fail: parseBool(core.getInput('fail')),
+  }
+}
+
+const options = parseInputs()
+runAction(options);
